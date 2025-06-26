0edb869ad26421281b58bec63f92f1af755e7e8a
Minhaj-Vora <144939750+Minhaj-Vora@users.noreply.github.com>
2025-06-26 07:58:23 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "Minhaj@google.com"
+var accountPassword = "12345"
+accountCity = "Ahemdabad"
+
+let accountState;
+
+// accountId = 2
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
