c6b0baec44f51114c1a78eeeed00cd5b7b798bed
Minhaj-Vora <144939750+Minhaj-Vora@users.noreply.github.com>
2025-06-27 03:48:21 +0000

    datatypes complated

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //teat all js code as newer version
+
+// alert(3+3) // we are using node js not browser
+
+console.log(3+3) //code readablity should high
+
+console.log("Minhaj")
+let name = "Minhaj"
+let age =18 
+let isLoggedIn = false 
+//number => 2 to power 53
+//bigInt
+//String =>
+//Boolean =>true/false
+//null => standalone value
+// undefined
+//symbol
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
