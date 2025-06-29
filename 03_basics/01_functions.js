0388dccf0c0e779c66645e25a2e1b0acd9d7c0b4
Minhaj-Vora <144939750+Minhaj-Vora@users.noreply.github.com>
2025-06-29 13:56:57 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -46,4 +46,38 @@ function loginUsermessage(username){
 }
 
 // console.log(loginUsermessage("samir"));
-console.log(loginUsermessage("minhaj"));
+// console.log(loginUsermessage("minhaj"));
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+
+
+
+// jo aa rite hoy to value one ma 200 jase value 2 ma 400 jase ane baki nu badhu num one ma jase rest oprator aapd ne ek array generate kari ne aapse
+// function calculateCartPrice(value1,value2,...num1){
+//     return num1
+// }
+
+
+// console.log(calculateCartPrice(200,400,600,2000));
+
+const user = {
+    username:"Minhaj",
+    price:199
+}
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+// handleObject(user);
+// handleObject({username:"juned",price:399});
+
+const mynewArray = [200,400,100,600];
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(mynewArray));
