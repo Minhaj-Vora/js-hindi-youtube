311a7eb14ceea8b4ef357ed6f6044a956a88eded
Minhaj-Vora <144939750+Minhaj-Vora@users.noreply.github.com>
2025-07-02 02:53:18 +0000

    IIFE Done

---
+++ 03_basics/04_list.js
@@ -0,0 +1,12 @@
+// Immediatly Invoked Function Expression (IIFE)
+
+// aama semicolon lagavavu jaruri chhe 
+(function chai(params) {
+    console.log(DB CONNECTED);
+    
+})();
+
+
+( (name) => {
+    console.log(DB CONNECTED ${name});
+})("Minhaj");
