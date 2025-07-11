103910f1eb78378a6391adf1d4264864fdc777b7
Minhaj-Vora <144939750+Minhaj-Vora@users.noreply.github.com>
2025-07-11 03:21:24 +0000

    promise

--- 9_advance_one/promises.js
+++ 9_advance_one/promises.js
@@ -68,3 +68,40 @@ const promiseFive = new Promise(function(resolve,reject){
         }
     }, 1000);
 })
+
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response    );
+    } catch (error) {
+        console.log(error);
+        
+    }
+    
+}
+
+consumePromiseFive()
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json()
+//         console.log(data);
+    
+//     } catch (error) {
+//         console.log("ERROR: ",error);
+//     }
+// }
+// getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data);
+    
+})
+.catch((error)=>console.log(error)
+)
